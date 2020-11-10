57aa1c6b4481b4681fefdf4f828669193b942328
AnsonAlexandr <AnsonAlexandr2@gmai.com>
2020-11-10 16:35:56 +0300

    lab-06

---
+++ lab-06.lua
@@ -0,0 +1,34 @@
+lgi = require 'ldi'
+sqlite = require 'lsqlite3'
+
+gtk = lgi.gethook
+pixbuf = lgi.GdkPixbuf.Pixbuf
+
+gtk.init()
+
+bld = gtk.Builder()
+bld:add_from_file('lab-06glade')
+
+ui - bld.objects
+
+rdrTxt = gtk.CellRendererText {}
+rdrPix = gtk.CellRendererPixbuf {}
+
+c1 = gtk.TreeViewColumn { title = 'Name', {rdrTxt, { text = 1 }}}
+c2 = gtk.TreeViewColumn { title = 'Value', {rdrTxt, { text = 2 }}}
+c3 = gtk.TreeViewColumn { title = 'Image', {rdrPix, { pixbuf = 3 }}}
+
+ui.lstItems:append_column(c1)
+ui.lstItems:append_column(c2)
+ui.lstItems:append_column(c3)
+
+db = sqlite.open('lab-06.db')
+
+for row in db:nrows("SELECT * FROM list") do
+  px = pixbuf.new_from_file(row.image)
+  
+  el = ui.storeItems:append()
+  ui.storeItems[el] = { [1] = row.name, [2] = row.value, [3] = px }
+end
+
+db:close()
---
+++ lab-06.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS list;
+CREATE TABLE list (Name Text, value INT, image TEXT);
+
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
+INSERT INTO list VALUES ('Apple', 10, 'img-apple.png');
